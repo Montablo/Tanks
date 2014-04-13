c6ed82b5ad94dc9c3283fa9e27de4e8873c0fe6a
cablej <jackhcable@gmail.com>
2014-04-13 13:57:34 -0500

    f

---
+++ Tanks/TanksGamePage.h
@@ -0,0 +1,13 @@
+//
+//  TanksGamePage.h
+//  Tanks
+//
+//  Created by greg.minter on 4/13/14.
+//  Copyright (c) 2014 Montablo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TanksGamePage : SKScene
+
+@end
---
+++ Tanks/TanksGamePage.m
@@ -0,0 +1,15 @@
+//
+//  TanksGamePage.m
+//  Tanks
+//
+//  Created by greg.minter on 4/13/14.
+//  Copyright (c) 2014 Montablo. All rights reserved.
+//
+
+#import "TanksGamePage.h"
+
+@implementation TanksGamePage
+
+-(void) 
+
+@end
